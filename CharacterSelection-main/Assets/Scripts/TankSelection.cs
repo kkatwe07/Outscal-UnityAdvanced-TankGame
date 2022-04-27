b823de3d6141e44cc6177a41e883e8513aae7149
Kishan Katwe <kishan.r.katwe123@gmail.com>
2022-04-27 11:18:55 +0530

    Track3-Chapter8-Assignment3
    UI Selection Panel

---
+++ CharacterSelection-main/Assets/Scripts/TankSelection.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankSelection : MonoBehaviour
+{
+
+    public TankSpawner tankSpawner;
+
+    public void BlueTankSelected()
+    {
+        tankSpawner.CreateTank(TankTypes.BlueTank);
+        this.gameObject.SetActive(false);
+    }
+
+    public void GreenTankSelected()
+    {
+        tankSpawner.CreateTank(TankTypes.GreenTank);
+        this.gameObject.SetActive(false);
+    }
+
+    public void RedTankSelected()
+    {
+        tankSpawner.CreateTank(TankTypes.RedTank);
+        this.gameObject.SetActive(false);
+    }
+}
